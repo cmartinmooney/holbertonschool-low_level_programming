3a00b1382a17cb0c160aa71e34609d68482877fa
Caleb <cmartinmooney@gmail.com>
2021-06-23 22:50:05 -0400

    l

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,15 +7,17 @@
  */
 int main(void)
 {
-int int1;
-float float2;
-double double3;
-char char4;
+char char1;
+int int2;
+long int longint3;
+long long int longlongint4;
+float float5;
 
-printf("Size of int: %zu bytes\n", sizeof(int1));
-printf("Size of float: %zu bytes\n", sizeof(float2));
-printf("Size of double: %zu bytes\n", sizeof(double3));
-printf("Size of char: %zu bytes\n", sizeof(char4));
+printf("Size of a char: %zu bytes\n", sizeof(char1));
+printf("Size of an int: %zu bytes\n", sizeof(int2));
+printf("Size of a long int: %zu bytes\n", sizeof(longint3));
+printf("Size of a long long int: %zu bytes\n", sizeof(longlongint4));
+printf("Size of a float: %zu bytes\n", sizeof(float5));
 
 return(0);
 }
