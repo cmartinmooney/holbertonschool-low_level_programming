caf944639ddd7fdfdcc051f2d2580413d1707b73
Caleb <cmartinmooney@gmail.com>
2021-06-23 22:56:22 -0400

    l

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ long int longint3;
 long long int longlongint4;
 float float5;
 
-printf("Size of a char: %zu bytes\n", sizeof(char1));
-printf("Size of an int: %zu bytes\n", sizeof(int2));
-printf("Size of a long int: %zu bytes\n", sizeof(longint3));
-printf("Size of a long long int: %zu bytes\n", sizeof(longlongint4));
-printf("Size of a float: %zu bytes\n", sizeof(float5));
+printf("Size of a char: %zu byte(s)\n", sizeof(char1));
+printf("Size of an int: %zu byte(s)\n", sizeof(int2));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longint3));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongint4));
+printf("Size of a float: %zu byte(s)\n", sizeof(float5));
 
 return(0);
 }
