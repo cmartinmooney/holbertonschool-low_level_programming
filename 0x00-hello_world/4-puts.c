23570090d481de28a47d798b11c47427fa7e3615
Caleb <cmartinmooney@gmail.com>
2021-06-23 21:21:59 -0400

    l

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    return (0);
+}
+
+{
+char myvar[]="\"Programming is like building a multilingual puzzle";
+
+puts(myvar);
+
+return(0);
