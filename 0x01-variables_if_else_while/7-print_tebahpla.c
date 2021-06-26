0d0d6282f5dfaed05c4c6e5bd43277f700626c6d
Caleb <cmartinmooney@gmail.com>
2021-06-25 23:14:30 -0400

    lol

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - program entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 25;
+	char str[] = "abcdefghijklmnopqrstuvwxyz";
+
+	while (str[i] >= 0)
+	{
+		putchar(str[i]);
+		i--;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
