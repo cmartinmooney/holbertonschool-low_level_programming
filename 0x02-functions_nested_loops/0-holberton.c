8a66ec040a5b6df971524c84e9c2521d0af1131e
Caleb <cmartinmooney@gmail.com>
2021-06-28 14:36:42 -0400

    k

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int main(void)
+{
+
+putchar('h');
+putchar('o');
+putchar('l');
+putchar('b');
+putchar('e');
+putchar('r');
+putchar('t');
+putchar('o');
+putchar('n');
+putchar('\n');
+return (0);
+
+}
