874c667de9f3decf42cfb5ee740b67b880509a21
Caleb <cmartinmooney@gmail.com>
2021-06-28 21:35:20 -0400

    k

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - checks for lowercase
+ * @c: character to check
+ *
+ * Return: 0 success, 1 fail
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && <= 'z') | (c >= 'A' && c <= 'Z'))
+			return(1);
+	return(0);
+}
