a3ddc75df376d37190961d987eb001e73e197d30
Caleb <cmartinmooney@gmail.com>
2021-06-28 22:38:45 -0400

    k

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_sign checks if lowercase
+ * *@n: char to check
+ *
+ * Return: 1 if more than 0, 0 if 0, -1 if below 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('0');
+		return (-1);
+	}
+
+}
