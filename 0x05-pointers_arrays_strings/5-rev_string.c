f66a99ceebb9cb1ebabad882aad5aec69110efba
Caleb <cmartinmooney@gmail.com>
2021-07-06 22:36:04 -0400

    k

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * rev_string - writes an array of chars in reverse
+ * @s: pointer to string
+ *
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = 0;
+	char hold;
+
+	while (s[i]) /* loop through char array by pointer */
+		i++;
+
+	i -= 1; /* i is now length of string */
+
+	while (j < i) /* loop until meet string length */
+	{
+		hold = s[j]; /* s[0] is now held */
+		s[j] = s[i]; /* s[0] is now last char */
+		s[i] = hold; /* last char is now first char */
+		j++; /* increment to s[1] */
+		i--; /* decrement to s[len-1] */
+	}
+}
+
