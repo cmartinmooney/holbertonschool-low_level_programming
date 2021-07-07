4add05ff99f7f8bdb9d1d501f0fb5488b89858a7
Caleb <cmartinmooney@gmail.com>
2021-07-06 22:29:20 -0400

    k

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swaps int a and b
+ * @a: pointer to int a
+ * @b: pointer to int b
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
