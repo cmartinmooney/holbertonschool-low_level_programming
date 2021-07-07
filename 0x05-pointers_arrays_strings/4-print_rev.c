86672c67c29ccac304da324860eaddf7073a1cb8
Caleb <cmartinmooney@gmail.com>
2021-07-06 22:35:08 -0400

    k

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_rev - writes an array of chars in reverse
+ * @str: pointer to string
+ *
+ */
+void print_rev(char *str)
+{
+	int i = 0;
+
+	while (str[i]) /* loop through char array by pointer */
+		i++;
+
+	i -= 1;
+	while (i >= 0)
+	{
+		_putchar(str[i]);
+		i--;
+	}
+	_putchar('\n');
+}
+
