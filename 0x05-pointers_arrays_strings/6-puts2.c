fdcc59c117c3059655b2a324fae8816c0c88c9e5
Caleb <cmartinmooney@gmail.com>
2021-07-06 22:37:11 -0400

    k

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * puts2 - writes every other char
+ * @str: pointer to string
+ *
+ */
+void puts2(char *str)
+{
+	int i = 0;
+	int j = 0;
+
+	while (str[i]) /* loop through char array by pointer */
+		i++;
+
+	for (j = 0; j < i; j += 2)
+		_putchar(str[j]);
+	_putchar('\n');
+}
