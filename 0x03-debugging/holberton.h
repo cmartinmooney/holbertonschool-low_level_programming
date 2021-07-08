c65886c601ecbff782ce8e0405b65179c8263975
Caleb <cmartinmooney@gmail.com>
2021-07-07 20:10:39 -0400

    k

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,13 +1,11 @@
-int _putchar(char c);
-  2 void print_alphabet(void);
-  3 void print_alphabet_x10(void);
-  4 int _islower(int c);
-  5 int _isalpha(int c);
-  6 int print_sign(int n);
-  7 int _abs(int n);
-  8 int print_last_digit(int n);
-  9 void jack_bauer(void);
- 10 void times_table(void);
- 11 int add(int a, int b);
- 12 void print_to_98(int n);
- 13 void print_times_table(int n);
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+
+#endif /* HOLBERTON_H */
