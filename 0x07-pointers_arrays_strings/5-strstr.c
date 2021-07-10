54629634e9abf9ca080a0a69a5ae8b1a2e6bda42
Caleb <cmartinmooney@gmail.com>
2021-07-09 23:26:43 -0400

    l

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * *_strstr - returns occurrence of needle in haystack
+ * @haystack: str to search
+ * @needle: substr to locate
+ *
+ * Return: Pointer to beginning of substr or null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+
+	if (!*needle)
+		return (haystack);
+
+	while (*haystack)
+	{
+		while (haystack[i] == needle[i])
+		{
+			if (!needle[i + 1])
+				return (haystack);
+
+			i++;
+		}
+		haystack++;
+	}
+
+	return (NULL);
+}
